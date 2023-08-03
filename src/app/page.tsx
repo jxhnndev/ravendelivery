03642a0c7704d2c20ed2158b4e13c78559a483bd
jxhnndev <jxhn.ysicca@gmail.com>
2023-08-03 14:40:13 +0200

    slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import { Featured, Offer, Slider } from '@/components'
 
 export default function Home() {
   return (
-    <main>Hello</main>
+    <main>
+      <Slider />
+      <Featured />
+      <Offer />
+    </main>
   )
 }
