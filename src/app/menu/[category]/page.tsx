26b29ad111c9418240076e805277baefcefad6cd
jxhnndev <jxhn.ysicca@gmail.com>
2023-08-03 20:23:50 +0200

    menu and category page

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { pizzas } from '@/data'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const CategoryPage = () => {
   return (
-    <div>CategoryPage</div>
+    <div className="flex flex-wrap text-gold">
+      {pizzas.map((item) => (
+        <Link className="w-full h-[60vh] border-r-2 border-b-2 border-gold sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group odd:bg-lightGold" href={`/product/${item.id}`} key={item.id}>
+          {/* IMAGE CONTAINER */}
+          {item.img && (
+            <div className="relative h-[80%]">
+              <Image src={item.img} alt="" fill className="object-contain"/>
+            </div>
+          )}
+          {/* TEXT CONTAINER */}
+          <div className="flex items-center justify-between font-bold">
+            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
+            <button className="hidden group-hover:block uppercase bg-gold text-white p-2 rounded-md">Add to Cart</button>
+          </div>
+        </Link>
+      ))}
+    </div>
   )
 }
 
